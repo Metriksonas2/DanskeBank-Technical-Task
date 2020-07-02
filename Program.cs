9318dd64198183daac4053ea3ebe1263738def42
Metriksonas2 <erikas.goriacevskis@gmail.com>
2020-07-02 11:08:06 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(4);
+        }
+
+        static void Print(int height)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+    }
+}
